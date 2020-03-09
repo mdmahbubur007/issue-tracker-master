9b7180f33781b29ff3d96c1b8de9694c0000a11e
mdmahbubur007 <61216155+mdmahbubur007@users.noreply.github.com>
2020-03-09 20:39:35 +0600

    add description value

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ const fetchIssues = () => {
     issuesList.innerHTML +=   `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
+                              <h3 class="${status === "Closed"? 'Closed': ''}"> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
                               <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
